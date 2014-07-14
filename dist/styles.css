a6669e9bd13a808dc52862b13fd7d4b23bcaf09f
bra1n <webmaster@artage.de>
2014-07-14 14:27:13 +0200

    Release v0.10.0

--- dist/styles.css
+++ dist/styles.css
@@ -1,4 +1,4 @@
-/*! mkmobile - v0.9.4 - 2014-07-14
+/*! mkmobile - v0.10.0 - 2014-07-14
  * Author: Steffen Baumgart <steffen@baumgart.biz>
  * https://github.com/bra1n/mkmobile
  * Copyright (c) 2014 ; Sammelkartenmarkt Ltd. & Co. KG */
